ea4c67de9253f87d7a878ce45ea4f4822c768e3f
Christo Aluckal <christo12aluckal@gmail.com>
2024-05-27 14:57:49 -0400

    trying cylindrical planning

---
+++ se3.cpp
@@ -0,0 +1,302 @@
+/*********************************************************************
+ * Software License Agreement (BSD License)
+ *
+ *  Copyright (c) 2010, Rice University
+ *  All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions
+ *  are met:
+ *
+ *   * Redistributions of source code must retain the above copyright
+ *     notice, this list of conditions and the following disclaimer.
+ *   * Redistributions in binary form must reproduce the above
+ *     copyright notice, this list of conditions and the following
+ *     disclaimer in the documentation and/or other materials provided
+ *     with the distribution.
+ *   * Neither the name of the Rice University nor the names of its
+ *     contributors may be used to endorse or promote products derived
+ *     from this software without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ *  FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ *  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ *  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ *  LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ *  CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ *  LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ *  ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ *  POSSIBILITY OF SUCH DAMAGE.
+ *********************************************************************/
+  
+ /* Author: Ioan Sucan */
+  
+ #include <ompl/base/SpaceInformation.h>
+ #include <ompl/base/spaces/SE3StateSpace.h>
+ #include <ompl/geometric/planners/prm/PRM.h>
+ #include <ompl/geometric/SimpleSetup.h>
+  
+ #include <ompl/config.h>
+ #include <iostream>
+ #include <vector>
+  
+ namespace ob = ompl::base;
+ namespace og = ompl::geometric;
+
+ class Circle
+ {
+    public:
+        float x;
+        float y;
+        float r;
+        Circle(float x, float y, float r)
+        {
+            this->x = x;
+            this->y = y;
+            this->r = r;
+        }
+
+        bool isInside(float x, float y)
+        {
+            return (x - this->x)*(x - this->x) + (y - this->y)*(y - this->y) <= this->r*this->r;
+        }
+};
+
+class Cylinder
+{
+    public:
+        float x;
+        float y;
+        float r;
+        float h;
+        Cylinder(float x, float y, float r, float h)
+        {
+            this->x = x;
+            this->y = y;
+            this->r = r;
+            this->h = h;
+        }
+
+        bool isInside(float x, float y, float z)
+        {
+            float dist = sqrt((x - this->x)*(x - this->x) + (y - this->y)*(y - this->y));
+            if (dist > this->r && z < this->h)
+            {
+                return true;
+            }
+            return false;
+        }
+};
+
+
+
+std::vector<Circle> circles;
+std::vector<Cylinder> cylinders;
+
+
+
+
+  
+ bool isStateValid(const ob::State *state)
+ {
+
+     // cast the abstract state type to the type we expect
+     const auto *se3state = state->as<ob::SE3StateSpace::StateType>();
+  
+     // extract the first component of the state and cast it to what we expect
+     const auto *pos = se3state->as<ob::RealVectorStateSpace::StateType>(0);
+  
+     // extract the second component of the state and cast it to what we expect
+     const auto *rot = se3state->as<ob::SO3StateSpace::StateType>(1);
+
+     // return a value that is always true but uses the two variables we define, so we avoid compiler warnings
+    //  return (const void*)rot != (const void*)pos;
+  
+     // check validity of state defined by pos & rot
+
+    // for (int i = 0; i < circles.size(); i++)
+    // {
+    //     if (circles[i].isInside(pos->values[0], pos->values[1]))
+    //     {
+    //         return false;
+    //     }
+    // }
+
+    for (int i = 0; i < cylinders.size(); i++)
+    {
+        std::vector<double> positions;
+        positions.push_back(pos->values[0]);
+        positions.push_back(pos->values[1]);
+        positions.push_back(pos->values[2]);
+
+        std::cout << "Checking " << positions[0] << " " << positions[1] << " " << positions[2] << std::endl;
+
+        if (cylinders[i].isInside(positions[0], positions[1], positions[2]))
+        {
+            return false;
+        }
+    }
+
+    return true;
+  
+  
+     
+ }
+  
+ void plan()
+ {
+     // construct the state space we are planning in
+     auto space(std::make_shared<ob::SE3StateSpace>());
+
+     std::cout << "A" << std::endl;
+  
+     // set the bounds for the R^3 part of SE(3)
+     ob::RealVectorBounds bounds(3);
+     bounds.setLow(-1);
+     bounds.setHigh(1);
+  
+     space->setBounds(bounds);
+
+     std::cout << "B" << std::endl;
+  
+     // construct an instance of  space information from this state space
+     auto si(std::make_shared<ob::SpaceInformation>(space));
+  
+     // set state validity checking for this space
+     si->setStateValidityChecker(isStateValid);
+  
+     // create a random start state
+     ob::ScopedState<> start(space);
+     start.random();
+  
+     // create a random goal state
+     ob::ScopedState<> goal(space);
+     goal.random();
+
+     
+  
+     // create a problem instance
+     auto pdef(std::make_shared<ob::ProblemDefinition>(si));
+  
+     // set the start and goal states
+     pdef->setStartAndGoalStates(start, goal);
+  
+     // create a planner for the defined space
+     auto planner(std::make_shared<og::PRM>(si));
+  
+     // set the problem we are trying to solve for the planner
+     planner->setProblemDefinition(pdef);
+
+     std::cout << "C" << std::endl;
+  
+     // perform setup steps for the planner
+     planner->setup();
+
+     std::cout << "D" << std::endl;
+  
+  
+     // print the settings for this space
+     si->printSettings(std::cout);
+  
+     // print the problem settings
+     pdef->print(std::cout);
+
+     std::cout << "E" << std::endl;
+  
+     // attempt to solve the problem within one second of planning time
+     ob::PlannerStatus solved = planner->ob::Planner::solve(1.0);
+
+     std::cout << "F" << std::endl;
+  
+     if (solved)
+     {
+         // get the goal representation from the problem definition (not the same as the goal state)
+         // and inquire about the found path
+         ob::PathPtr path = pdef->getSolutionPath();
+         std::cout << "Found solution:" << std::endl;
+  
+         // print the path to screen
+         path->print(std::cout);
+     }
+     else
+         std::cout << "No solution found" << std::endl;
+ }
+  
+ void planWithSimpleSetup()
+ {
+     // construct the state space we are planning in
+     auto space(std::make_shared<ob::SE3StateSpace>());
+  
+     // set the bounds for the R^3 part of SE(3)
+     ob::RealVectorBounds bounds(3);
+     bounds.setLow(-10);
+     bounds.setHigh(10);
+  
+     space->setBounds(bounds);
+  
+     // define a simple setup class
+     og::SimpleSetup ss(space);
+  
+     // set state validity checking for this space
+     ss.setStateValidityChecker([](const ob::State *state) { return isStateValid(state); });
+  
+     // create a random start state
+     ob::ScopedState<> start(space);
+     start.random();
+    start[0] = 0.0;
+    start[1] = 0.0;
+    start[2] = 1.0;
+
+  
+     // create a random goal state
+     ob::ScopedState<> goal(space);
+     goal.random();
+    goal[0] = 4.5;
+    goal[1] = 4.5;
+    goal[2] = 1.0;
+  
+     // set the start and goal states
+     ss.setStartAndGoalStates(start, goal);
+  
+     // this call is optional, but we put it in to get more output information
+     ss.setup();
+     ss.print();
+  
+     // attempt to solve the problem within one second of planning time
+     ob::PlannerStatus solved = ss.solve(1.0);
+  
+     if (solved)
+     {
+         std::cout << "Found solution:" << std::endl;
+         // print the path to screen
+         ss.simplifySolution();
+         ss.getSolutionPath().print(std::cout);
+     }
+     else
+         std::cout << "No solution found" << std::endl;
+ }
+  
+ int main(int /*argc*/, char ** /*argv*/)
+ {
+    
+    Cylinder c1(1,1,1,10000);
+    Cylinder c2(2,2,1,10000);
+    Cylinder c3(3,3,1,10000); 
+
+    cylinders.push_back(c1);
+    cylinders.push_back(c2);
+    cylinders.push_back(c3);
+
+    std::cout << "OMPL version: " << OMPL_VERSION << std::endl;
+  
+    //  plan();
+  
+     std::cout << std::endl << std::endl;
+  
+     planWithSimpleSetup();
+  
+     return 0;
+ }
